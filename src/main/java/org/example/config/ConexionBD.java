16ef54f6c4e0551593cb254e5d1063b3f7357ee8
Giovanny Alejandro Tapiero Cataño <giovanny.tapiero@pi.edu.co>
2026-08-08 17:54:36 -0500

    commit - Operacion .

--- src/main/java/org/example/config/ConexionBD.java
+++ src/main/java/org/example/config/ConexionBD.java
@@ -30,5 +30,5 @@ public class ConexionBD {
 
             return null;
         }
-    }      
+    }
 }
---
+++ src/main/java/org/example/modelo/Operacion.java
@@ -0,0 +1,66 @@
+package org.example.modelo;
+
+public class Operacion {
+
+    private int id;
+    private double numero1;
+    private double numero2;
+    private String operacion;
+    private double resultado;
+
+    // constructor vacio.
+    public Operacion() {
+
+    }
+
+    public Operacion(double numero1,
+                     double numero2,
+                     String operacion,
+                     double resultado) {
+
+        this.numero1 = numero1;
+        this.numero2 = numero2;
+        this.operacion = operacion;
+        this.resultado = resultado;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getNumero1() {
+        return numero1;
+    }
+
+    public void setNumero1(double numero1) {
+        this.numero1 = numero1;
+    }
+
+    public double getNumero2() {
+        return numero2;
+    }
+
+    public void setNumero2(double numero2) {
+        this.numero2 = numero2;
+    }
+
+    public String getOperacion() {
+        return operacion;
+    }
+
+    public void setOperacion(String operacion) {
+        this.operacion = operacion;
+    }
+
+    public double getResultado() {
+        return resultado;
+    }
+
+    public void setResultado(double resultado) {
+        this.resultado = resultado;
+    }   
+}
